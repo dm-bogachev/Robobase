67a8a2d9139b49b63a539615de7ecd2c39f7f506
Bogachev Dmitrii <dm.bogachev@yandex.ru>
2022-08-16 18:13:52 +0300

    smol

--- database/models/robot.py
+++ database/models/robot.py
@@ -8,18 +8,49 @@ class Robot(models.Model):
     # Historical records default model
     history = HistoricalRecords()
 
-    name = models.CharField(max_length=255,
-                            unique=True,
-                            verbose_name='Имя',
-                            db_index=True,)
+    location = models.ForeignKey('Location',
+                                 on_delete=models.SET_DEFAULT,
+                                 verbose_name='Местоположение',
+                                 blank=True,
+                                 null=True,
+                                 default=1,)  # default = 'Неизвестно'
+
+    client = models.ForeignKey('Client',
+                               on_delete=models.SET_DEFAULT,
+                               verbose_name='Клиент',
+                               blank=True,
+                               null=True,
+                               default=1,)  # default = 'Неизвестно'
+
+    integrator = models.ForeignKey('Integrator',
+                                   on_delete=models.SET_DEFAULT,
+                                   verbose_name='Интегратор',
+                                   blank=True,
+                                   null=True,
+                                   default=1,)  # default = 'Неизвестно'
+
+    arm = models.ForeignKey('RobotArm',
+                            on_delete=models.SET_DEFAULT,
+                            verbose_name='Модель робота',
+                            default=1,)  # default = 'Неизвестно'
 
     arm_sn = models.CharField(max_length=32,
                               verbose_name='Серийный номер робота',
                               db_index=True,)
 
+    controller = models.ForeignKey('RobotController',
+                                   on_delete=models.SET_DEFAULT,
+                                   verbose_name='Контроллер',
+                                   default=1,)  # default = 'Неизвестно'
+
     controller_sn = models.CharField(max_length=32,
                                      verbose_name='Серийный номер контроллера',
                                      db_index=True,)
+
+    name = models.CharField(max_length=255,
+                            unique=True,
+                            verbose_name='Имя',
+                            db_index=True,)
 
     description = models.TextField(verbose_name='Комментарий',
                                    blank=True,
@@ -36,37 +67,6 @@ class Robot(models.Model):
                                   verbose_name='Удалённый',
                                   db_index=True,)
 
-    controller = models.ForeignKey('RobotController',
-                                   on_delete=models.SET_DEFAULT,
-                                   verbose_name='Контроллер',
-                                   default=1,)  # default = 'Неизвестно'
-
-    arm = models.ForeignKey('RobotArm',
-                            on_delete=models.SET_DEFAULT,
-                            verbose_name='Модель робота',
-                            default=1,)  # default = 'Неизвестно'
-
-    client = models.ForeignKey('Client',
-                               on_delete=models.SET_DEFAULT,
-                               verbose_name='Клиент',
-                               blank=True,
-                               null=True,
-                               default=1,)  # default = 'Неизвестно'
-
-    integrator = models.ForeignKey('Integrator',
-                                   on_delete=models.SET_DEFAULT,
-                                   verbose_name='Интегратор',
-                                   blank=True,
-                                   null=True,
-                                   default=1,)  # default = 'Неизвестно'
-
-    location = models.ForeignKey('Location',
-                                 on_delete=models.SET_DEFAULT,
-                                 verbose_name='Текущее местоположение',
-                                 blank=True,
-                                 null=True,
-                                 default=1,) # default = 'Неизвестно'
-
     added_by = models.ForeignKey(settings.AUTH_USER_MODEL,
                                  on_delete=models.SET_DEFAULT,
                                  verbose_name='Кем добавлен',
@@ -74,11 +74,10 @@ class Robot(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     # def save(self, request, *args, **kwargs) -> None:
     #     self.added_by = request.user
     #     return super().save(*args, **kwargs)
-
 
     class Meta:
         verbose_name_plural = 'Роботы'
